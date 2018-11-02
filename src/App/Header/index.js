d0730006174ec954fdd2aa4f2978d69a8a164349
Stan Zhang <stan.zhang2@gmail.com>
2018-11-02 17:59:04 -0400

    unified menu for account options

--- src/App/Header/index.js
+++ src/App/Header/index.js
@@ -5,15 +5,41 @@
  */
 
 import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
-import Octicon, { Plus, Person } from '@githubprimer/octicons-react';
+import { string, node } from 'prop-types';
+import { withRouter, NavLink } from 'react-router-dom';
+import Octicon, { SignIn, Plus, Person } from '@githubprimer/octicons-react';
 
 import User from 'src/User';
-import LoginLink from 'src/Login/Link';
 import Logout from 'src/Logout';
 import DropdownNav from './DropdownNav';
 
 import styles from './index.less';
+
+/**
+ * Button-based link component.
+ *
+ * @private
+ *
+ * @param {Object} props - The component's props.
+ * @param {Object} props.history - Router history object.
+ * @param {string} props.to - The location to link to.
+ * @param {node} props.children - The component's children.
+ * @returns {ReactElement} The component's elements.
+ */
+const ButtonLink = withRouter(function(props) {
+    const { history, to, children } = props;
+
+    return <button onClick={() => {
+        history.push(to);
+    }}>
+        {children}
+    </button>;
+});
+
+ButtonLink.propTypes = {
+    to: string.isRequired,
+    children: node
+};
 
 /**
  * Logo component.
@@ -41,24 +67,32 @@ function Logo() {
  * @returns {ReactElement} The component's elements.
  */
 function Account() {
-    if (!User.loggedIn) {
-        return <nav>
-            <Link to="/signup/">
-                <Octicon icon={Plus} />
-                &nbsp;Sign up
-            </Link>
-            <LoginLink />
-        </nav>;
-    }
-
     const title = [
         <Octicon key='icon' icon={Person} />,
         <span key='text'>&nbsp;Account</span>
     ];
 
+    let menu;
+    if (User.loggedIn) {
+        menu = [
+            <Logout key="logout" />
+        ];
+    } else {
+        menu = [
+            <ButtonLink key="login" to="/login/">
+                <Octicon icon={SignIn} />
+                &nbsp;Log in
+            </ButtonLink>,
+            <ButtonLink key="signup" to="/signup/">
+                <Octicon icon={Plus} />
+                &nbsp;Sign up
+            </ButtonLink>
+        ];
+    }
+
     return <nav>
         <DropdownNav title={title}>
-            <Logout />
+            {menu}
         </DropdownNav>
     </nav>;
 }
