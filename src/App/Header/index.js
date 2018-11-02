abdfa6122249165e14c3f9a4fb38289378f05df8
Stan Zhang <stan.zhang2@gmail.com>
2018-11-02 16:59:45 -0400

    Factor out dropdown nav

--- src/App/Header/index.js
+++ src/App/Header/index.js
@@ -5,15 +5,13 @@
  */
 
 import React from 'react';
-import { object, string, node, bool } from 'prop-types';
-import { Route, Link, NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import Octicon, { Plus, Person } from '@githubprimer/octicons-react';
-import classNames from 'classnames';
 
 import User from 'src/User';
-import Dropdown from 'src/Dropdown';
 import LoginLink from 'src/Login/Link';
 import Logout from 'src/Logout';
+import DropdownNav from './DropdownNav';
 
 import styles from './index.less';
 
@@ -34,91 +32,6 @@ function Logo() {
         <div className={styles.text} />
     </NavLink>;
 }
-
-/**
- * Dropdown button React component.
- *
- * @param {Object} props - The component's props.
- * @param {string} props.to - The dropdown's target path. Used for routing.
- * @param {ReactNode} props.title - The title for the dropdown.
- * @param {boolean} props.isOpen - Whether or not the dropdown is open.
- * @returns {ReactElement} The component's elements.
- */
-function DropdownButton(props) {
-    const { to, title, isOpen } = props;
-
-    /**
-     * Button React component.
-     *
-     * @param {Object} buttonProps - The component's props.
-     * @param {boolean} match - Whether or not the target path has been matched.
-     * @returns {ReactElement} The component's elements.
-     */
-    function Button(buttonProps) {
-        const { match } = buttonProps;
-        const classes = classNames(styles.button, {
-            [styles.active]: match !== null,
-            [styles.open]: isOpen
-        });
-
-        return <button
-            className={classes}
-            onClick={event => event.preventDefault()}
-        >
-            {title}
-        </button>;
-    }
-
-    Button.propTypes = {
-        match: object
-    };
-
-    return <Route path={to}>
-        {Button}
-    </Route>;
-}
-
-DropdownButton.propTypes = {
-    to: string.isRequired,
-    title: node.isRequired,
-    isOpen: bool
-};
-
-/**
- * Dropdown menu component.
- *
- * @private
- *
- * @param {Object} props - The component's props.
- * @param {ReactNode} props.title - The title for the dropdown.
- * @param {ReactNode} props.children - The dropdown items.
- * @returns {ReactElement} The component's elements.
- */
-function DropdownMenu(props) {
-    const { title, children } = props;
-    const { enter, enterActive, exit, exitActive } = styles;
-
-    return <Dropdown
-        className={styles.dropdown}
-        button={<DropdownButton to='/account/' title={title} />}
-        transition={{
-            appear: true,
-            classNames: {
-                enter, enterActive, exit, exitActive,
-                appear: enter,
-                appearActive: enterActive
-            },
-            timeout: 300
-        }}
-    >
-        {children}
-    </Dropdown>;
-}
-
-DropdownMenu.propTypes = {
-    title: node.isRequired,
-    children: node.isRequired
-};
 
 /**
  * Account toolbar component.
@@ -143,22 +56,11 @@ function Account() {
         <span key='text'>&nbsp;Account</span>
     ];
 
-    /**
-     * Menu items.
-     *
-     * @private
-     *
-     * @returns {ReactElement} The component's elements.
-     */
-    function Menu() {
-        return <div className={styles.menu}>
+    return <nav>
+        <DropdownNav title={title}>
             <Logout />
-        </div>;
-    }
-
-    return <DropdownMenu title={title}>
-        <Menu />
-    </DropdownMenu>;
+        </DropdownNav>
+    </nav>;
 }
 
 /**
