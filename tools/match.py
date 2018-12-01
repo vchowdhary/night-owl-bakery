5715f46b4610f00124b3329b3aa6427b336cd11d
Stan Zhang <stan.zhang2@gmail.com>
2018-12-01 17:17:16 -0500

    ML tools

---
+++ tools/match.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# match.py
+
+import pickle
+import pandas as pd
+from sklearn_pandas import DataFrameMapper
+
+employeePath = 'data/employees.csv'
+
+model = pickle.load(open('data/model.pickle', 'rb'))
+employeeData = pd.read_csv(employeePath, sep=',')
+employeeData['merge'] = 1
+
+def match(employerPath, limit=10):
+    employerData = pd.read_csv(employerPath, sep=',')
+    employerData['merge'] = 1
+    df = pd.merge(employerData, employeeData, on='merge')
+    del df['merge']
+    attrs = set(df.columns.values)
+    ignoredAttrs = set([
+        'id_x',
+        'id_y',
+        'nameFirst_x',
+        'nameFirst_y',
+        'nameLast_x',
+        'nameLast_y',
+        'origin_x',
+        'origin_y'
+    ])
+    inputAttrs = list(attrs - ignoredAttrs)
+    inputsMap = DataFrameMapper([
+        (inputAttrs, None)
+    ])
+
+    inputSamples = inputsMap.fit_transform(df)
+
+    result = pd.DataFrame({ 'id': df['id_y'] })
+    result['score'] = model.predict(inputSamples)
+
+    return result
+        .sort_values(by='score', ascending=False)
+        .truncate(after=limit)
+
---
+++ tools/svm.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# svm.py
+
+import pickle
+import numpy as np
+import pandas as pd
+from sklearn.svm import SVR
+from sklearn_pandas import DataFrameMapper
+
+# Data file paths
+employerPath = 'data/employers.csv'
+employeePath = 'data/employees.csv'
+reviewPath = 'data/reviews.csv'
+
+# Data file parsing
+employerData = pd.read_csv(employerPath, sep=',', index_col='id')
+employeeData = pd.read_csv(employeePath, sep=',', index_col='id')
+reviewData = pd.read_csv(reviewPath, sep=',')
+
+df = reviewData
+df = df.join(employerData, on='employerID')
+df = df.join(employeeData, on='employeeID', rsuffix='_e')
+
+# Feature selection
+attrs = set(df.columns.values)
+ignoredAttrs = set([
+    'employerID',
+    'employeeID',
+    'nameFirst',
+    'nameFirst_e',
+    'nameLast',
+    'nameLast_e',
+    'origin',
+    'origin_e'
+])
+outputAttr = 'score'
+inputAttrs = list(attrs - ignoredAttrs - set([outputAttr]))
+inputsMap = DataFrameMapper([
+    (inputAttrs, None)
+])
+
+# Model training
+inputSamples = inputsMap.fit_transform(df)
+outputSamples = list(df[outputAttr].values)
+
+model = SVR(kernel='rbf', gamma='scale')
+model.fit(inputSamples, outputSamples)
+
+pickle.dump(model, open('data/model.pickle', 'wb'))
+
